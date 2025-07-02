65b9461cc95c0b39172e853e0f08b902a4d665e0
ejahanz <88173908+ejahanz@users.noreply.github.com>
2025-07-03 07:06:05 +1200

    commit

--- main.tf
+++ main.tf
@@ -13,11 +13,12 @@ resource "azurerm_storage_account" "storage" {
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+}
 
-  static_website {
-    index_document     = "index.html"
-    error_404_document = "error.html"
-  }
+resource "azurerm_storage_account_static_website" "website" {
+  storage_account_id = azurerm_storage_account.storage.id
+  index_document     = "index.html"
+  error_404_document = "error.html"
 }
 
 resource "random_id" "unique" {
