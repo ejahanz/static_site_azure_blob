03aeaadd3e1a800340adf4ffdfd1249fe2d9bb17
ejahanz <88173908+ejahanz@users.noreply.github.com>
2025-07-04 13:55:41 +1200

    update main.tf and output.tf

--- main.tf
+++ main.tf
@@ -28,39 +28,34 @@ resource "random_id" "unique" {
 
 # azurerm_storage_static_website.storage
 
-# Create CDN Profile
-
+# --- CDN resources commented out due to subscription restrictions ---
 resource "azurerm_cdn_profile" "cdn" {
   name                = "cdnprofile-${random_id.unique.hex}"
-  location            = azurerm_resource_group.rg.location
+  location            = "global"
   resource_group_name = azurerm_resource_group.rg.name
   sku                 = "Standard_Microsoft"
 }
 
-# CDN Endpoint pointing to the static website
-
 resource "azurerm_cdn_endpoint" "cdn_endpoint" {
   name                = "cdnendpoint-${random_id.unique.hex}"
   profile_name        = azurerm_cdn_profile.cdn.name
-  location            = azurerm_resource_group.rg.location
+  location            = "global"
   resource_group_name = azurerm_resource_group.rg.name
   is_http_allowed     = false
   is_https_allowed    = true
-  origin_host_header  = azurerm_storage_account.storage.primary_web_endpoint
+  origin_host_header = "staticsite88c8e1d2.z44.web.core.windows.net"
   origin_path         = ""
   origin {
     name      = "blobstaticorigin"
-    host_name = azurerm_storage_account.storage.primary_web_endpoint
+    host_name = "staticsite88c8e1d2.z44.web.core.windows.net"
     https_port = 443
   }
   content_types_to_compress = ["text/html", "text/css", "application/javascript"]
   is_compression_enabled    = true
 }
 
-# Map Custom Domain to CDN
-
 resource "azurerm_cdn_endpoint_custom_domain" "cdn_custom_domain" {
   name            = "cdncustomdomain"
   cdn_endpoint_id = azurerm_cdn_endpoint.cdn_endpoint.id
-  host_name       = "https://azureblob.cloudkraft.nz"
+  host_name       = "azureblob.cloudkraft.nz"
 }
--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
-output "static_website_url" {
-  value = azurerm_storage_account.storage.primary_web_endpoint
-}
+# output "static_website_url" {
+#   value = azurerm_storage_account.storage.primary_web_endpoint
+# }
 
 output "cdn_endpoint" {
-  value = azurerm_cdn_endpoint.cdn_endpoint.host_names[0]
+  value = azurerm_cdn_endpoint.cdn_endpoint.fqdn
 }
 
-output "custom_domain_url" {
-  value = "https://azureblob.cloudkraft.nz"
-}
+#  output "custom_domain_url" {
+#    value = "http://azureblob.cloudkraft.nz"
+#  } 
